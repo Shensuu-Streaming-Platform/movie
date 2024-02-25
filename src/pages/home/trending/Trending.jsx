2abfeb2117e2eb0ac28a1c1da1df78be5b1877f0
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-02-25 23:40:01 +0800

    Tending to All

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -9,7 +9,7 @@ import useFetch from "../../../hooks/useFetch";
 const Trending = () => {
     const [endpoint, setEndpoint] = useState("day");
 
-    const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
+    const { data, loading } = useFetch(`/trending/all/${endpoint}`);
 
     const onTabChange = (tab) => {
         setEndpoint(tab === "Day" ? "day" : "week");
