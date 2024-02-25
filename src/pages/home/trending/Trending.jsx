3b7ce89ee929f7922ac64fa3492396cbf946c964
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-02-26 00:04:10 +0800

    Default week

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -7,19 +7,19 @@ import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import useFetch from "../../../hooks/useFetch";
 
 const Trending = () => {
-    const [endpoint, setEndpoint] = useState("day");
+    const [endpoint, setEndpoint] = useState("week");
 
     const { data, loading } = useFetch(`/trending/all/${endpoint}`);
 
     const onTabChange = (tab) => {
-        setEndpoint(tab === "Day" ? "day" : "week");
+        setEndpoint(tab === "Week" ? "week" : "day");
     };
 
     return (
         <div className="carouselSection">
             <ContentWrapper>
                 <span className="carouselTitle">Trending</span>
-                <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+                <SwitchTabs data={["Week", "Day"]} onTabChange={onTabChange} />
             </ContentWrapper>
             <Carousel data={data?.results} loading={loading} />
         </div>
