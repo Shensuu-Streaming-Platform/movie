a54ed283299a0fa8924b0b769a1338931be9f26a
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-09 11:21:33 +0800

    Popular to Week

--- src/pages/home/popular/Popular.jsx
+++ src/pages/home/popular/Popular.jsx
@@ -9,7 +9,7 @@ import useFetch from "../../../hooks/useFetch";
 const Popular = () => {
     const [endpoint, setEndpoint] = useState("movie");
 
-    const { data, loading } = useFetch(`/trending/${endpoint}/day`); {/* /${endpoint}/popular */}
+    const { data, loading } = useFetch(`/trending/${endpoint}/week`); {/* /${endpoint}/popular */}
 
     const onTabChange = (tab) => {
         setEndpoint(tab === "Movies" ? "movie" : "tv");
