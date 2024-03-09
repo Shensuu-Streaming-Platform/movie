8c00f96d717d6fa88619ab6fe0c3ce97d951e946
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-09 20:31:21 +0800

    Server Status

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,13 +14,14 @@ const Footer = () => {
     return (
         <footer className="footer">
             <ContentWrapper>
-			{/* <ul className="menuItems">
-                    <li className="menuItem">Terms Of Use</li>
+			    <ul className="menuItems">
+                {/*    <li className="menuItem">Terms Of Use</li>
                     <li className="menuItem">Privacy-Policy</li>
                     <li className="menuItem">About</li>
                     <li className="menuItem">Blog</li>
-                    <li className="menuItem">FAQ</li>
-				</ul> */}
+					<li className="menuItem">FAQ</li> */}
+					<a href="https://stats.uptimerobot.com/y8sQL00Dtj" > <li className="menuItem">Server Status</li> </a>
+				</ul>
 				<div className="infoText">
 					<h3>Disclaimer</h3>
 						<p>Shensuu Movie does not host any files, it merely links to 3rd party services. Legal issues should be taken up with the file hosts and providers. Shensuu Movie is not responsible for any media files shown by the video providers.
