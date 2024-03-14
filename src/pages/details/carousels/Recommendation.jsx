9c02518e68a2058a0b02fa41112e6b29789e53f3
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-14 09:11:41 +0800

    Remake Recomendations

--- src/pages/details/carousels/Recommendation.jsx
+++ src/pages/details/carousels/Recommendation.jsx
@@ -3,18 +3,24 @@ import React from "react";
 import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
+import "./style.scss";
+
 const Recommendation = ({ mediaType, id }) => {
     const { data, loading, error } = useFetch(
         `/${mediaType}/${id}/recommendations`
     );
 
     return (
-        <Carousel
-            title="Recommendations"
-            data={data?.results}
-            loading={loading}
-            endpoint={mediaType}
-        />
+		<div className="carouselSection">
+            <ContentWrapper>
+                <span className="carouselTitle">Recommendations</span>
+            </ContentWrapper>
+            <Carousel
+				data={data?.results}
+				loading={loading}
+				endpoint={mediaType}
+			/>
+        </div>
     );
 };
 
