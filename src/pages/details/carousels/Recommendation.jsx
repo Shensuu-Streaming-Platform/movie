989a1d4fe81231fafb538638bd3086bac70015f7
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-14 09:14:30 +0800

    Fix bug

--- src/pages/details/carousels/Recommendation.jsx
+++ src/pages/details/carousels/Recommendation.jsx
@@ -3,24 +3,18 @@ import React from "react";
 import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
-import "./style.scss";
-
 const Recommendation = ({ mediaType, id }) => {
     const { data, loading, error } = useFetch(
         `/${mediaType}/${id}/recommendations`
     );
 
     return (
-		<div className="carouselSection">
-            <ContentWrapper>
-                <span className="carouselTitle">Recommendations</span>
-            </ContentWrapper>
-            <Carousel
-				data={data?.results}
-				loading={loading}
-				endpoint={mediaType}
-			/>
-        </div>
+        <Carousel
+            title="Recommendations"
+            data={data?.results}
+            loading={loading}
+            endpoint={mediaType}
+        />
     );
 };
 
