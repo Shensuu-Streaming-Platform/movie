7e79e986da98a7b49fe82932718ccb2c7d3994a1
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-16 15:08:29 +0800

    new test 1

--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -37,51 +37,53 @@ const Cast = ({ data, loading }) => {
     };
 
     return (
-        <div className="castSection">
-            <ContentWrapper>
-                <div className="sectionHeading">Top Cast</div>
-                <div className="scrollButtons">
-                    <BsFillArrowLeftCircleFill
-                        className="arrow leftArrow"
-                        onClick={() => navigation("left")}
-                    />
-                    <BsFillArrowRightCircleFill
-                        className="arrow rightArrow"
-                        onClick={() => navigation("right")}
-                    />
-                </div>
-                {!loading ? (
-                    <div className="listItems" ref={castListRef}>
-                        {data?.map((item) => {
-                            let imgUrl = item.profile_path
-                                ? url.profile + item.profile_path
-                                : avatar;
-                            return (
-                                <div key={item.id} className="listItem">
-                                    <div className="profileImg">
-                                        <Img src={imgUrl} />
-                                    </div>
-                                    <div className="name">{item.name}</div>
-                                    <div className="character">
-                                        {item.character}
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                ) : (
-                    <div className="castSkeleton">
-                        {skeleton()}
-                        {skeleton()}
-                        {skeleton()}
-                        {skeleton()}
-                        {skeleton()}
-                        {skeleton()}
-                    </div>
-                )}
-            </ContentWrapper>
-        </div>
-    );
+		<div className="castSection">
+			<ContentWrapper>
+				<div className="sectionHeading">
+					Top Cast
+					<div className="scrollButtons">
+						<BsFillArrowLeftCircleFill
+							className="arrow"
+							onClick={() => navigation("left")}
+						/>
+						<BsFillArrowRightCircleFill
+							className="arrow"
+							onClick={() => navigation("right")}
+						/>
+					</div>
+				</div>
+				{!loading ? (
+					<div className="listItems" ref={castListRef}>
+						{data?.map((item) => {
+							let imgUrl = item.profile_path
+								? url.profile + item.profile_path
+								: avatar;
+							return (
+								<div key={item.id} className="listItem">
+									<div className="profileImg">
+										<Img src={imgUrl} />
+									</div>
+									<div className="name">{item.name}</div>
+									<div className="character">
+										{item.character}
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				) : (
+					<div className="castSkeleton">
+						{skeleton()}
+						{skeleton()}
+						{skeleton()}
+						{skeleton()}
+						{skeleton()}
+						{skeleton()}
+					</div>
+				)}
+			</ContentWrapper>
+		</div>
+	);
 };
 
 export default Cast;
