fecb0d7e88c0b760e96a9148f72faa6c1d1106c1
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-16 15:40:58 +0800

    Added scrool button in videos

--- src/pages/details/videosSection/VideosSection.jsx
+++ src/pages/details/videosSection/VideosSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./style.scss";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import VideoPopup from "../../../components/videoPopup/VideoPopup";
@@ -8,6 +8,21 @@ import { PlayIcon } from "../Playbtn";
 const VideosSection = ({ data, loading }) => {
     const [show, setShow] = useState(false);
     const [videoId, setVideoId] = useState(null);
+    const castListRef = useRef(null);
+
+    const navigation = (dir) => {
+        const container = castListRef.current;
+
+        const scrollAmount =
+            dir === "left"
+                ? container.scrollLeft - (container.offsetWidth + 10)
+                : container.scrollLeft + (container.offsetWidth + 10);
+
+        container.scrollTo({
+            left: scrollAmount,
+            behavior: "smooth",
+        });
+    };
 
     const loadingSkeleton = () => {
         return (
@@ -22,49 +37,59 @@ const VideosSection = ({ data, loading }) => {
     const filteredVideos = data?.results?.filter(video => video.type === "Trailer");
 
     return (
-        <div className="videosSection">
-            <ContentWrapper>
-                {!loading && data?.results?.length > 0 && (
-                    <div className="sectionHeading">Videos</div>
-                )}
-                {!loading ? (
-                    <div className="videos">
-                        {filteredVideos?.map((video) => (
-                            <div
-                                key={video.id}
-                                className="videoItem"
-                                onClick={() => {
-                                    setVideoId(video.key);
-                                    setShow(true);
-                                }}
-                            >
-                                <div className="videoThumbnail">
-                                    <Img
-                                        src={`https://img.youtube.com/vi/${video.key}/mqdefault.jpg`}
-                                    />
-                                    <PlayIcon />
-                                </div>
-                                <div className="videoTitle">{video.name}</div>
-                            </div>
-                        ))}
-                    </div>
-                ) : (
-                    <div className="videoSkeleton">
-                        {loadingSkeleton()}
-                        {loadingSkeleton()}
-                        {loadingSkeleton()}
-                        {loadingSkeleton()}
-                    </div>
-                )}
-            </ContentWrapper>
-            <VideoPopup
-                show={show}
-                setShow={setShow}
-                videoId={videoId}
-                setVideoId={setVideoId}
-            />
-        </div>
-    );
+		<div className="videosSection">
+			<ContentWrapper>
+				<div className="sectionHeading">
+					Videos
+					<div className="scrollButtons">
+						<BsFillArrowLeftCircleFill
+							className="arrow"
+							onClick={() => navigation("left")}
+						/>
+						<BsFillArrowRightCircleFill
+							className="arrow"
+							onClick={() => navigation("right")}
+						/>
+					</div>
+				</div>
+				{!loading ? (
+					<div className="videos" ref={castListRef}>
+						{filteredVideos?.map((video) => (
+							<div
+								key={video.id}
+								className="videoItem"
+								onClick={() => {
+									setVideoId(video.key);
+									setShow(true);
+								}}
+							>
+								<div className="videoThumbnail">
+									<Img
+										src={`https://img.youtube.com/vi/${video.key}/mqdefault.jpg`}
+									/>
+									<PlayIcon />
+								</div>
+								<div className="videoTitle">{video.name}</div>
+							</div>
+						))}
+					</div>
+				) : (
+					<div className="videoSkeleton">
+						{loadingSkeleton()}
+						{loadingSkeleton()}
+						{loadingSkeleton()}
+						{loadingSkeleton()}
+					</div>
+				)}
+			</ContentWrapper>
+			<VideoPopup
+				show={show}
+				setShow={setShow}
+				videoId={videoId}
+				setVideoId={setVideoId}
+			/>
+		</div>
+	);
 };
 
 export default VideosSection;
