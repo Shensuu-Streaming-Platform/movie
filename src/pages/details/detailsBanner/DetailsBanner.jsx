96415041b58f478c909031c581cb7d97ff90fdb0
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-22 17:28:32 +0800

    go back t2

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -57,7 +57,7 @@ const DetailsBanner = ({ video, crew }) => {
                             </div>
                             <div className="opacity-layer"></div>
                             <ContentWrapper>
-								<div>
+								<div className="content">
 									<span className="goback" onClick={goBackPage}>
 										<i class="bi bi-arrow-left"></i>Go Back
 									</span>
