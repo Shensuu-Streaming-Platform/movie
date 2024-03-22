ac1ec50ee9960b78245b796aef21703bbd05d2df
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-22 20:15:45 +0800

    Fix Auto title 2

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import dayjs from "dayjs";
