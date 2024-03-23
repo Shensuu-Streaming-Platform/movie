4dbc466d231068c94cea600938ea1980427c1387
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-23 21:11:44 +0800

    Update VideoPlayer.jsx

--- src/pages/playing/VideoPlayer.jsx
+++ src/pages/playing/VideoPlayer.jsx
@@ -22,23 +22,25 @@ const VideoPlayer = () => {
     const iframeUrl = `https://shensuumovie.8888008.xyz/#/media/tmdb-${mediaType}-${id}`;
 
     return (
-		<div className="videogoback">
-			<span className="gbackbutton" onClick={goBackPage}>
-				<i class="bi bi-arrow-bar-left"></i> Go Back
-			</span>
-		</div>
-        <div className="videoPlayer">
-            <ContentWrapper>
-                <div className="videoWrapper">
-                    <iframe 
-                        title="Shensuu Movie"
-                        src={iframeUrl}
-                        frameBorder="0"
-                        allowFullScreen
-                    />
-                </div>
-            </ContentWrapper>
-        </div>
+        <>
+			<div className="videoPlayer">
+				<ContentWrapper>
+					<div className="videogoback">
+						<span className="gbackbutton" onClick={goBackPage}>
+							<i class="bi bi-arrow-bar-left"></i> Go Back
+						</span>
+					</div>
+					<div className="videoWrapper">
+						<iframe 
+							title="Shensuu Movie"
+							src={iframeUrl}
+							frameBorder="0"
+							allowFullScreen
+						/>
+					</div>
+				</ContentWrapper>
+			</div>
+		</>
     );
 };
 
