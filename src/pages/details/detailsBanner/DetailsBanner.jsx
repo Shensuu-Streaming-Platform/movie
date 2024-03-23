2d0d9b8ed9bf527a2964e56f552c038ce19a5d76
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-23 11:32:30 +0800

    Change Player

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -38,7 +38,7 @@ const DetailsBanner = ({ video, crew }) => {
 	
 	const watchNow = () => {
 		{/*	const url = `https://player.8888008.xyz/?type=${mediaType}&id=${id}`; */}
-		const url = `https://movie.8888008.xyz/player.html?type=${mediaType}&id=${id}`;
+		const url = `https://movie.8888008.xyz/player3.html?type=${mediaType}&id=${id}`;
 		window.location.href = url;	{/* window.open(url, '_blank'); */}
 	};
 	
