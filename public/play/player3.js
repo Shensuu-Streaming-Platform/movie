15b9b5e274fde1e05de25913cca2cab94fec8d8a
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-26 22:04:17 +0800

    Update player3.js

--- public/play/player3.js
+++ public/play/player3.js
@@ -34,12 +34,12 @@ fetch(apiUrl)
     });
 
 // Append the logo to the video-container div
-// var logoContainer = document.createElement('div');
-// logoContainer.classList.add('logo-container');
+var logoContainer = document.createElement('div');
+logoContainer.classList.add('logo-container');
 
-// var logoImg = document.createElement('img');
-// logoImg.setAttribute('src', '/movix-logo.svg'); 
-// logoImg.setAttribute('alt', 'Shensuu Movie');
+var logoImg = document.createElement('img');
+logoImg.setAttribute('src', '/movix-logo.svg'); 
+logoImg.setAttribute('alt', 'Shensuu Movie');
 
 logoImg.addEventListener('click', function() {
     window.location.href = 'https://movie.8888008.xyz/' + type + '/' + id;
