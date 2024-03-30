a4620c5eef88eb3e6ae1c1d58a2c1b54fda7ef7b
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-30 19:06:47 +0800

    Update Details.jsx

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -21,8 +21,8 @@ const Details = () => {
             <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
             <Cast data={credits?.cast} loading={creditsLoading} />
 			<VideosSection data={data} loading={loading} />
-			<Similar mediaType={mediaType} id={id} />
             <Recommendation mediaType={mediaType} id={id} />
+			<Similar mediaType={mediaType} id={id} />
         </div>
     );
 };
