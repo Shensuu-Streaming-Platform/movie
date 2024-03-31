fe016c93bceab6bbf83162732cd22324f4d289dd
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-03-31 18:40:31 +0800

    Update SeasonEpisodes.jsx

--- src/pages/details/showEpisodes/SeasonEpisodes.jsx
+++ src/pages/details/showEpisodes/SeasonEpisodes.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./style.scss";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import { useSelector } from "react-redux";
 
 const SeasonEpisodes = ({ mediaType, id }) => {
     const [seasons, setSeasons] = useState([]);
