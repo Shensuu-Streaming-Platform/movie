3fcd77d3e53a5291aa003046d055951a8f218f54
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-04-04 18:01:43 +0800

    Update Similar.jsx

--- src/pages/details/carousels/Similar.jsx
+++ src/pages/details/carousels/Similar.jsx
@@ -6,7 +6,7 @@ import useFetch from "../../../hooks/useFetch";
 const Similar = ({ mediaType, id }) => {
     const { data, loading, error } = useFetch(`/trending/${mediaType}/week`); {/*  /${mediaType}/${id}/similar */}
 
-    const title = mediaType === "tv" ? "Discover TV Shows" : "Discover Movies";
+    const title = mediaType === "tv" ? "Discover Series" : "Discover Movies";
 
     return (
 		<>
