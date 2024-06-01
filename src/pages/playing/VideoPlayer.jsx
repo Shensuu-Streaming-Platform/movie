894793b838dffd81dc1287f558e7bef07867c7da
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-06-01 14:50:31 +0800

    fix test 1 player title

--- src/pages/playing/VideoPlayer.jsx
+++ src/pages/playing/VideoPlayer.jsx
@@ -59,10 +59,9 @@ const VideoPlayer = () => {
                         />
                     </div>
 					<div className="playtitle">
-						{`${
-							titleData.name || titleData.title
-						}`}
-					</div>
+                        {titleData?.name || titleData?.title}
+                        {mediaType === "tv" && ` S${seasonNumber} E${episodeNumber}`}
+                    </div>
                 </ContentWrapper>
                 <SeasonEpisodes mediaType={mediaType} id={id} />
             </div>
