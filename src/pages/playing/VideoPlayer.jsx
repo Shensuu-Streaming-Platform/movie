6445fc0b8f4d551fd368b98675df0caa5161c183
Sheyste Benerable <86936264+sheyste@users.noreply.github.com>
2024-10-16 11:38:30 +0800

    Update VideoPlayer.jsx

--- src/pages/playing/VideoPlayer.jsx
+++ src/pages/playing/VideoPlayer.jsx
@@ -48,7 +48,7 @@ const VideoPlayer = () => {
     // Construct the iframe URL
     const iframeUrl = mediaType === "movie"
         ? `https://embed.su/embed/${mediaType}/${id}`
-        : `https://embed.su/embed/${mediaType}/${id}${seasonNumber ? `-${seasonNumber}` : ""}${episodeNumber ? `-${episodeNumber}` : ""}`;
+        : `https://embed.su/embed/${mediaType}/${id}${seasonNumber ? `/${seasonNumber}` : ""}${episodeNumber ? `/${episodeNumber}` : ""}`;
 
 	const networkLogo = titleData?.networks?.[0]?.logo_path;
 
